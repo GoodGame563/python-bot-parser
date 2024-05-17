7128c56699c98a5d05ba26201fafb9a6e1920250
GoodGame563 <124364912+GoodGame563@users.noreply.github.com>
2024-05-17 18:59:46 +0300

    не большая перестановка файлов

--- news_aggregator/config.py
+++
@@ -1,9 +0,0 @@
-# Параметры из my.telegram.org
-api_id = 22011075
-api_hash = "06d9e5be825c6a7c6dce2ed077ac87f0"
-
-# Бот из @BotFather
-bot_token = "7064443400:AAGYVAtnIDmJRGEnZ89hzzqjYvKJV5zNAZI"
-
-# id канала куда будут сливаться все новости
-gazp_chat_id = -1002096816791
